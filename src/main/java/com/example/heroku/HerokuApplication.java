e37708105aea22a9fe64ddf1187fde6601902723
t-haumas <thethomasricks@gmail.com>
2024-03-21 18:13:20 -0600

    Implement logging my name

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -62,6 +62,8 @@ public class HerokuApplication {
       stmt.executeUpdate("INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + getRandomString() + "')");
       ResultSet rs = stmt.executeQuery("SELECT tick, random_string FROM table_timestamp_and_random_string");
 
+      System.out.println("Thomas Ricks");
+
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
         output.add("Read from DB: " + rs.getTimestamp("tick") + " " + rs.getString("random_string"));
